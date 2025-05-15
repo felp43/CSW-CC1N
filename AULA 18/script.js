6ab3ec93e9887c92c8ac0fc9d65aa65973fb0182
felp43 <feliphe.dalcol@gmail.com>
2025-05-14 22:35:18 -0300

    Add files via upload

---
+++ AULA 18/script.js
@@ -0,0 +1,18 @@
+  type="text/JavaScript">
+    document.write("<h2>Olá, Mundo! Esse é o meu primeiro código de JavaScript</h2> <br>");
+    document.write("<p>Eu amo JavaSprict</p> <br>");
+    // window.alert("Pop Up");
+    document.getElementById("subtitulo").innerHTML = "Esse é o meu segundo programa JavaScript --> " + new Date + "<br>";
+    // document.getElementById("resposta").innerHTML = window.confirm("Você está gostando do curso?")
+    // window.prompt("Qual é o seu nome?")
+    document.getElementById("resposta").innerHTML = "O nome digitado foi: " + window.prompt("Qual seu nome?")
+
+    function abrePagina()
+    {
+        window.open("novocodigojs.html", "_blank")
+    }
+
+    function mudaCor()
+    {
+        document.getElementById
+    }
---
+++ AULA 18/script2.js
@@ -0,0 +1,9 @@
+function enviaResposta() 
+{
+   var IU = document.getElementById('inputUsuario').value;
+   window.alert('Foi digitado ' + IU);
+}
+function exibeDados()
+{
+    window.alert('Nome digitado foi: '+ document.getElementById("nome").value);
+}
