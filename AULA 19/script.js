12a0edd24c1242e2d0e635c4bc8e3d5b7ca93b3c
felp43 <feliphe.dalcol@gmail.com>
2025-05-21 22:30:40 -0300

    Add files via upload

--- AULA 19/script.js
+++ AULA 19/script.js
@@ -1,18 +1,29 @@
 function verificaridade() {
     var idade = parseInt(document.getElementById("idade").value)
     var resultado = document.getElementById("resultado")
-if (idade <= 10) {resultado.textContent = "Você é criança!"}
-else if (idade > 10 && idade < 15) 
+if (idade < 0) 
+    {
+        resultado.textContent = "Idade inválida!"
+    }
+else if (idade == 0)
+    {
+        resultado.textContent = "Você é recém nascido"
+    }
+else if (idade > 0 && idade < 10) 
+    {
+        resultado.textContent = "Você é criança!"
+    }
+else if (idade >= 10 && idade < 16) 
     {
         resultado.textContent = "Você é adolescente!"
     }
-else if (idade > 15 && idade < 21) 
+else if (idade >= 16  && idade < 25) 
     {
-        resultado.textContent = "Você é Jovem!"
+        resultado.textContent = "Você é jovem!"
     }
-else if (idade >= 21 && idade < 60) 
+else if (idade >= 25 && idade < 60)
     {
-        resultado.textContent = "Você ta velho!"
+        resultado.textContent = "Você é adulto!"
     }
 else if (idade >= 60)
     {
